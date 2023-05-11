7d095bb19407e28246b83bbd1317d1407de2a394
Lucas <lucaslatessa@gmail.com>
2023-05-11 03:32:03 -0300

    Creacion de educacion

--- src/main/java/com/portfolio/lucas/Controller/PersonaController.java
+++ src/main/java/com/portfolio/lucas/Controller/PersonaController.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 //@RequestMapping("/personas")
-@CrossOrigin(origins = {"http://localhost:4200"})
+@CrossOrigin(origins = {"https://frontendlucas-64482.web.app/","http://localhost:4200"})
 public class PersonaController {
     @Autowired
     IPersonaService ipersonaService;
