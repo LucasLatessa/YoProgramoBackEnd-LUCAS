f0193516b0ee173bde5c8dde3b936494b55700f1
Lucas <lucaslatessa@gmail.com>
2023-05-10 23:52:54 -0300

    agrego archivo dockerfile para dploy en render

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM amazoncorretto:1.8
+
+MAINTAINER lucaslatessa
+
+COPY target/lucas-0.0.1-SNAPSHOT.jar lucas-0.0.1-SNAPSHOT.jar
+
+ENTRYPOINT ["java","-jar","/lucas-0.0.1-SNAPSHOT.jar"]
+
+ 
---
+++ src/main/java/com/portfolio/lucas/Service/SEducacion.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.lucas.Service;
+
+import com.portfolio.lucas.Entity.Educacion;
+import com.portfolio.lucas.Repository.REducacion;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author lucas
+ */
+@Service
+@Transactional
+public class SEducacion {
+    @Autowired
+    REducacion rEducacion;
+    
+    public List<Educacion> list(){
+        return rEducacion.findAll();
+    }
+    
+    public Optional<Educacion> getOne(int id){
+        return rEducacion.findById(id);
+    }
+    
+    public Optional<Educacion> getByNmbreE(String nombreE){
+        return rEducacion.findByNombreE(nombreE);
+    }
+    
+    public void save(Educacion educacion){
+        rEducacion.save(educacion);
+    }
+    
+    public void delete(int id){
+        rEducacion.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return rEducacion.existsById(id);
+    }
+    
+    public boolean existsByNombreE(String nombreE){
+        return rEducacion.existsByNombreE(nombreE);
+    }
+}
+
