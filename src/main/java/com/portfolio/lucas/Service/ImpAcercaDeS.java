3831d23977d96ce9895d57653d8aea2f80e177eb
Lucas <lucaslatessa@gmail.com>
2023-05-13 01:23:45 -0300

    add acercade

---
+++ src/main/java/com/portfolio/lucas/Service/ImpAcercaDeS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.lucas.Service;
+
+import com.portfolio.lucas.Entity.AcercaDe;
+import com.portfolio.lucas.Repository.IAcercaDeRepo;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author lucas
+ */
+@Service
+@Transactional
+public class ImpAcercaDeS {
+    @Autowired
+    IAcercaDeRepo iAcercaDeRepo;
+    public Optional<AcercaDe> getOne(int id){
+        return iAcercaDeRepo.findById(id);
+    }
+public void save(AcercaDe acercaDe){
+        iAcercaDeRepo.save(acercaDe);
+}
+}
